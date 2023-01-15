d4743514129ac7b4aafc0b0bd377fca1b847c5da
coofzilla <jeric614@yahoo.com>
2023-01-15 10:33:25 +0900

    feature: implement color change in response to right or wrong answer and update progress bar to reflect changes

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -5,7 +5,7 @@
 //  Created by Angela Yu on 12/07/2019.
 //  Copyright © 2019 The App Brewery. All rights reserved.
 //
-
+import Foundation
 import UIKit
 
 class ViewController: UIViewController {
@@ -16,12 +16,23 @@ class ViewController: UIViewController {
     @IBOutlet weak var falseButton: UIButton!
     
     let questions = [
-        Question(text: "Swift is a compiled programming language.", answer: "True"),
-        Question(text:"Swift is an object-oriented language.", answer: "True"),
-        Question(text: "Swift was developed by Google", answer: "False"),
+        Question(q: "A slug's blood is green.", a: "True"),
+        Question(q: "Approximately one quarter of human bones are in the feet.", a: "True"),
+        Question(q: "The total surface area of two human lungs is approximately 70 square metres.", a: "True"),
+        Question(q: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", a: "True"),
+        Question(q: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", a: "False"),
+        Question(q: "It is illegal to pee in the Ocean in Portugal.", a: "True"),
+        Question(q: "You can lead a cow down stairs but not up stairs.", a: "False"),
+        Question(q: "Google was originally called 'Backrub'.", a: "True"),
+        Question(q: "Buzz Aldrin's mother's maiden name was 'Moon'.", a: "True"),
+        Question(q: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", a: "False"),
+        Question(q: "No piece of square dry paper can be folded in half more than 7 times.", a: "False"),
+        Question(q: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", a: "True")
+        
     ]
     
     var questionNumber = 0
+    var timer: Timer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,9 +44,9 @@ class ViewController: UIViewController {
         let actualAnswer = questions[questionNumber].answer.lowercased()
         
         if userAnswer == actualAnswer {
-            print("Right")
+            sender.backgroundColor = UIColor.green
         } else {
-            print("Wrong")
+            sender.backgroundColor = UIColor.red
         }
         
         let quizEnd = questionNumber == questions.count - 1
@@ -44,11 +55,14 @@ class ViewController: UIViewController {
         } else {
             questionNumber += 1
         }
-        updateUI()
+        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
     }
     
-    func updateUI(){
+    @objc func updateUI(){
         questionLabel.text = questions[questionNumber].text
+        trueButton.backgroundColor = UIColor.clear
+        falseButton.backgroundColor = UIColor.clear
+        progressBar.progress = Float(questionNumber + 1)/Float(questions.count)
     }
 }
 
